69f655b4c5b5b70cf27264dcf3e351265a37dd0f
Simon Prickett <simon.prickett@grafana.com>
2025-06-02 18:26:20 +0100

    First attempt.

---
+++ src/pages/HelloWorld/helloWorldScene.ts
@@ -0,0 +1,171 @@
+import { EmbeddedScene, SceneDataTransformer, SceneFlexLayout, SceneFlexItem, SceneQueryRunner, PanelBuilders } from '@grafana/scenes';
+
+export function helloWorldScene() {
+  const queryRunner1 = new SceneQueryRunner({
+    datasource: {
+      type: 'yesoreyeram-infinity-datasource',
+      uid: 'carbon-intensity-data-source'
+    },
+    queries: [
+      {
+        refId: 'A',
+        url: 'https://api.carbonintensity.org.uk/regional',
+        method: 'GET',
+        source: 'url',
+        parser: 'uql',
+        uql: `
+          parse-json|scope "data[0].regions"
+          | project "regionid", "shortname", "generationmix", "intensity"
+          | mv-expand "fuel_data"="generationmix"
+          | extend "intensity"="intensity.index", "fuel"="fuel_data.fuel", "percentage"="fuel_data.perc"
+          | project-away "fuel_data"
+          | order by "regionid" asc
+        `,
+      },
+    ],
+  });
+
+  // Get the intensity for North Scotland...
+  const transformer1 = new SceneDataTransformer({
+    $data: queryRunner1,
+    transformations: [
+      {
+        'id': 'filterByValue',
+        'options': {
+          'filters': [
+            {
+              'config': {
+                'id': 'equal',
+                'options': {
+                  'value': 'North Scotland'
+                }
+              },
+              'fieldName': 'shortname'
+            }
+          ],
+          'match': 'any',
+          'type': 'include'
+        }
+      }
+    ]
+  });
+
+  // And the  intensity for South Wales...
+  const transformer2 = new SceneDataTransformer({
+    $data: queryRunner1,
+    transformations: [
+      {
+        'id': 'filterByValue',
+        'options': {
+          'filters': [
+            {
+              'config': {
+                'id': 'equal',
+                'options': {
+                  'value': 'South Wales'
+                }
+              },
+              'fieldName': 'shortname'
+            }
+          ],
+          'match': 'any',
+          'type': 'include'
+        }
+      }
+    ]
+  });
+
+  return new EmbeddedScene({
+    $data: queryRunner1,
+    body: new SceneFlexLayout({
+      direction: 'column',
+      children: [
+        new SceneFlexItem({
+          width: '100%',
+          height: 150,
+          body: PanelBuilders.text().setTitle('Simon Panel').setOption('content', 'Well would you look at that.').build(),
+        }),
+        new SceneFlexItem({
+          width: '20%',
+          height: 150,
+          body: 
+            PanelBuilders
+              .stat()
+              .setTitle('North Scotland')
+              // TODO configure the colour values?
+              .setOption('reduceOptions', {
+                calcs: [
+                  'lastNotNull'
+                ],
+                fields: 'intensity',                    
+              })
+              .setData(transformer1)
+              .build(),
+        }),
+        new SceneFlexItem({
+          width: '20%',
+          height: 150,
+          body:           
+            PanelBuilders
+              .piechart()
+              .setTitle('North Scotland')
+              .setOption('reduceOptions', {
+                calcs: [
+                  'lastNotNull'
+                ],
+                fields: 'percentage',
+                values: true
+              })
+              // TODO set pieType to donut
+              .setOption('legend', {
+                showLegend: false
+              })
+              .setData(transformer1)
+              .build(),
+        }),
+        new SceneFlexItem({
+          width: '20%',
+          height: 150,
+          body: 
+            PanelBuilders
+              .stat()
+              .setTitle('South Wales')
+              .setOption('reduceOptions', {
+                calcs: [
+                  'lastNotNull'
+                ],
+                fields: 'intensity',                    
+              })
+              .setData(transformer2)
+              .build(),
+        }),
+        new SceneFlexItem({
+          width: '20%',
+          height: 150,
+          body:           
+            PanelBuilders
+              .piechart()
+              .setTitle('South Wales')
+              .setOption('reduceOptions', {
+                calcs: [
+                  'lastNotNull'
+                ],
+                fields: 'percentage',
+                values: true
+              })
+              // TODO set pieType to donut
+              .setOption('legend', {
+                showLegend: false
+              })
+              .setData(transformer2)
+              .build(),
+        }),
+        new SceneFlexItem({
+          width: '100%',
+          height: 600,
+          body: PanelBuilders.table().setTitle('Data').setData(queryRunner1).build(),
+        })
+      ],
+    }),
+  });
+}
